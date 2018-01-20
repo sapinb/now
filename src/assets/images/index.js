2830822df4727c45b0e7772563493bde0da6e552
Sapin Bajracharya <sbnull@gmail.com>
2018-01-20 16:33:00 +0545

    add images

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+export default {
+  arcticIce: () => require('./arctic-ice.jpg'),
+  lighthouse: () => require('./lighthouse.jpg'),
+  logoWave: () => require('./logo-wave.png'),
+  logo: () => require('./logo.png'),
+  seaRock: () => require('./sea-rock.jpg'),
+}
