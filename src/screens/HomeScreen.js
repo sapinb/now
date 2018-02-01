46ed5678c56d4c37e6b741c0d3363f82a28c4708
Sapin Bajracharya <sbnull@gmail.com>
2018-02-01 22:53:16 +0545

    fixup homescreen articles

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import { ForDesktopUp } from '../components/MediaQuery'
 import { HEADER_HEIGHT_PX } from '../components/Header'
 import { FullArticle, HalfArticle } from '../components/Article'
 
@@ -17,68 +17,95 @@ const Container = styled.div`
   padding-top: ${HEADER_HEIGHT_PX}px;
 `
 
+const articles = [
+  {
+    image: images.seaRock(),
+    title: 'Astronauts could land on Red Planet by 2039',
+    source: 'SPACE.com',
+    time: '20m ago',
+    channel: 'SCIENCE',
+    main: true,
+  },
+  {
+    image: images.arcticIce(),
+    title: 'Arctic sea ice extent hits record low for winter maximum',
+    source: 'CNN',
+    time: '1h ago',
+    channel: 'ENVIRONMENT',
+  },
+  {
+    image: images.phone(),
+    title: 'New battery for smartphones can be charged in a minute',
+    source: 'Reuters',
+    time: '6h ago',
+    channel: 'TECHNOLOGY',
+  },
+  {
+    image: images.forestRacoon(),
+    title: 'How wild Animals live in the National Forest',
+    source: 'SPACE.com',
+    time: '20m ago',
+    channel: 'ENVIRONMENT',
+  },
+  {
+    image: images.seaRock(),
+    title: 'Astronauts could land on Red Planet by 2039',
+    source: 'SPACE.com',
+    time: '20m ago',
+    channel: 'SCIENCE',
+  },
+  {
+    image: images.arcticIce(),
+    title: 'Arctic sea ice extent hits record low for winter maximum',
+    source: 'CNN',
+    time: '1h ago',
+    channel: 'ENVIRONMENT',
+    main: true,
+  },
+  {
+    image: images.phone(),
+    title: 'New battery for smartphones can be charged in a minute',
+    source: 'Reuters',
+    time: '6h ago',
+    channel: 'TECHNOLOGY',
+  },
+  {
+    image: images.forestRacoon(),
+    title: 'How wild Animals live in the National Forest',
+    source: 'SPACE.com',
+    time: '20m ago',
+    channel: 'ENVIRONMENT',
+  },
+]
+
 class HomeScreen extends React.Component {
+  _renderArticle = (article, idx) => (
+    <ForDesktopUp key={idx}>
+      {isDesktopUp => {
+        if (isDesktopUp) {
+          // only use idx
+          if (idx % 4 === 1) {
+            return <HalfArticle {...article} />
+          }
+          if (idx % 4 === 2) {
+            return <HalfArticle {...article} right />
+          }
+          return <FullArticle {...article} />
+        } else {
+          // use main
+          if (article.main || idx === 0) {
+            return <FullArticle {...article} />
+          }
+          return <HalfArticle {...article} key={idx} right={idx % 2} />
+        }
+      }}
+    </ForDesktopUp>
+  )
+
   render () {
     return (
       <Container>
-        <FullArticle
-          image={images.seaRock()}
-          title='Astronauts could land on Red Planet by 2039'
-          source='SPACE.com'
-          time='20m ago'
-          channel='SCIENCE'
-        />
-        <HalfArticle
-          image={images.arcticIce()}
-          title='Arctic sea ice extent hits record low for winter maximum'
-          source='CNN'
-          time='1h ago'
-          channel='ENVIRONMENT'
-          right
-        />
-        <HalfArticle
-          image={images.phone()}
-          title='New battery for smartphones can be charged in a minute'
-          source='Reuters'
-          time='6h ago'
-          channel='TECHNOLOGY'
-        />
-        <FullArticle
-          image={images.forestRacoon()}
-          title='How wild Animals live in the National Forest'
-          source='SPACE.com'
-          time='20m ago'
-          channel='ENVIRONMENT'
-        />
-        <FullArticle
-          image={images.seaRock()}
-          title='Astronauts could land on Red Planet by 2039'
-          source='SPACE.com'
-          time='20m ago'
-          channel='SCIENCE'
-        />
-        <HalfArticle
-          image={images.arcticIce()}
-          title='Arctic sea ice extent hits record low for winter maximum'
-          source='CNN'
-          time='1h ago'
-          channel='ENVIRONMENT'
-          right
-        />
-        <HalfArticle
-          image={images.phone()}
-          title='New battery for smartphones can be charged in a minute'
-          source='Reuters'
-          time='6h ago'
-          channel='TECHNOLOGY'
-        />
-        <FullArticle
-          image={images.forestRacoon()}
-          title='How wild Animals live in the National Forest'
-          source='SPACE.com'
-          time='20m ago'
-          channel='ENVIRONMENT'
-        />
+        {articles.map(this._renderArticle)}
       </Container>
     )
   }
